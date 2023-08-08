45726bc8ceaede532cf66988b97ad260aec96d29
jonas-45 <jonaswullah@gmail.com>
2023-08-08 10:08:28 +0000

    Define root url in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   authenticated :user do
-    root to "category#index", as: :authenticated_root
+    root "category#index", as: :authenticated_root
   end
 
   unauthenticated :user do
-    root to "splash#index", as: :unauthenticated_root
+    root "splash#index", as: :unauthenticated_root
   end
 
   resources :users
